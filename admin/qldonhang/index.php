8d3d5723503d4370ede79cdc08dc65e06613c4b2
nthikimngoc <ngocnguyen44008@gmail.com>
2023-12-14 09:20:02 -0600

    index của quan ly don hang

---
+++ admin/qldonhang/index.php
@@ -0,0 +1,39 @@
+<?php
+if (!isset($_SESSION["nguoidung"]))
+    header("location:../index.php");
+
+require("../../model/database.php");
+require("../../model/nguoidung.php");
+require("../../model/quyen.php");
+require("../../model/donhang.php");
+
+
+// Xét xem có thao tác nào được chọn
+if (isset($_REQUEST["action"])) {
+    $action = $_REQUEST["action"];
+} else {   // mặc định là xem danh sách
+    $action = "xem";
+}
+
+$dh = new DONHANG();
+$nd = new NGUOIDUNG();
+
+switch ($action) {
+    case "xem":
+        $donhang = $dh->laydonhang();
+        $nguoidung = $nd->laydanhsachnguoidung();
+
+        include("main.php");
+        break;
+    case "them":
+        $donhang = $pq->laydonhang();
+
+        include("add.php");
+        break;
+    case "xulythem":
+       
+        break;
+   
+    default:
+        break;
+}
