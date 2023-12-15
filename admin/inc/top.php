c2bf28fc1777d76e42adfcbb3c6617da6ff320c7
lephung02 <ltmphung_21th@student.agu.edu.vn>
2023-12-15 23:36:47 +0700

    index

--- admin/inc/top.php
+++ admin/inc/top.php
@@ -33,7 +33,7 @@
                     </li>
 
                     <li class="sidebar-item <?php if (strpos($_SERVER["REQUEST_URI"], "ktnguoidung") != false) echo "active"; ?>">
-						<a class="sidebar-link" href="../ktnguoidung/main.php">
+						<a class="sidebar-link" href="../ktnguoidung/index.php">
 							<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Bảng điều khiển</span>
 						</a>
 					</li>
