4d84e2d86bd728eac5a1fd623f3e049c0e89a2ec
lephung02 <ltmphung_21th@student.agu.edu.vn>
2023-12-15 21:18:02 +0700

    gioithieu

--- public/index.php
+++ public/index.php
@@ -31,6 +31,10 @@ switch ($action) {
         $sanpham = $sp->laysanpham();
         include("main.php");
         break;
+    case "gioithieu":
+        $sanpham = $sp->laysanpham();
+        include("gioithieu.php");
+        break;
     case "group":
         if (isset($_REQUEST["id"])) {
             $mapl = $_REQUEST["id"];
