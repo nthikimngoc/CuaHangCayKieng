f052f7642679c0e92665e02fa877a34a75b44292
lamhuy142 <147523526+lamhuy142@users.noreply.github.com>
2023-12-16 10:51:44 +0700

    dk

--- admin/ktnguoidung/index.php
+++ admin/ktnguoidung/index.php
@@ -22,7 +22,7 @@ $pl = new PHANLOAI();
 switch ($action) {
     case "macdinh":
         $sanphamhh = $sp->laysanphamhethang();
-        
+
         include("main.php");
         break;
     case "dangnhap":
@@ -54,78 +54,79 @@ switch ($action) {
         $diachi = $_POST["txtdiachi"];
         $hoten = $_POST["txthoten"];
         $hinhanh = $_POST["txthinhanh"];
-    
-        if($_FILES["fhinhanh"]["name"] != null) {
+
+        if ($_FILES["fhinhanh"]["name"] != null) {
             $hinhanh = basename($_FILES["fhinhanh"]["name"]);
             $duongdan = "../../images/users/" . $hinhanh;
             move_uploaded_file($_FILES["fhinhanh"]["tmp_name"], $duongdan);
         }
-        $nd->capnhatnguoidung($mand,$email,$sodt,$hoten,$hinhanh,$diachi);
+        $nd->capnhatnguoidung($mand, $email, $sodt, $hoten, $hinhanh, $diachi);
         $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($email);
         include("profile.php");
         break;
-        case "sua":
-            if (isset($_GET["id"])) {
-                $s = $sp->laysanphamtheoid($_GET["id"]);
-                $phanloai = $pl->layphanloai();
-                include("update.php");
-            } else {
-                $sanpham = $sp->laysanpham();
-                include("main.php");
-            }
-            break;
-        case "xulysua": // lưu dữ liệu sửa mới vào db
-    
-            // gán dữ liệu từ form
-            $spsua = new SANPHAM();
-            $spsua->setid($_POST["txtid"]);
-            $spsua->setmota($_POST["txtmota"]);
-            $spsua->setphanloaisp($_POST["optphanloai"]);
-            $spsua->settensp($_POST["txttensp"]);
-            $spsua->setgiaban($_POST["txtgiaban"]);
-            $spsua->setsoluongton($_POST["txtsoluongton"]);
-            $spsua->sethinhanh($_POST["txthinhcu"]);
-            $spsua->setluotxem($_POST["txtluotxem"]);
-            $spsua->setluotmua($_POST["txtluotmua"]);
-    
-            if ($_FILES["filehinhanh"]["name"] != "") {
-                //xử lý load ảnh
-                $hinhanh = "images/products/" . basename($_FILES["filehinhanh"]["name"]); // đường dẫn ảnh lưu trong db
-                $spsua->sethinhanh($hinhanh);
-                $duongdan = "../../" . $hinhanh; //nơi lưu file upload
-                move_uploaded_file($_FILES["filehinhanh"]["tmp_name"], $duongdan);
-            }
-            // sửa
-            $sp->suasanpham($spsua);
-            // load danh sách
-            $sanphamhh = $sp->laysanphamhethang();
+    case "sua":
+        if (isset($_GET["id"])) {
+            $s = $sp->laysanphamtheoid($_GET["id"]);
+            $phanloai = $pl->layphanloai();
+            include("update.php");
+        } else {
+            $sanpham = $sp->laysanpham();
             include("main.php");
-            break;
-        case "dangky":
-            include("register.php");
-            break;
-        case "xldangky":
-            $loai = $_POST["txtloai"];
+        }
+        break;
+    case "xulysua": // lưu dữ liệu sửa mới vào db
+
+        // gán dữ liệu từ form
+        $spsua = new SANPHAM();
+        $spsua->setid($_POST["txtid"]);
+        $spsua->setmota($_POST["txtmota"]);
+        $spsua->setphanloaisp($_POST["optphanloai"]);
+        $spsua->settensp($_POST["txttensp"]);
+        $spsua->setgiaban($_POST["txtgiaban"]);
+        $spsua->setsoluongton($_POST["txtsoluongton"]);
+        $spsua->sethinhanh($_POST["txthinhcu"]);
+        $spsua->setluotxem($_POST["txtluotxem"]);
+        $spsua->setluotmua($_POST["txtluotmua"]);
+
+        if ($_FILES["filehinhanh"]["name"] != "") {
             //xử lý load ảnh
-            $hinhanh = basename($_FILES["fhinhanh"]["name"]); // đường dẫn ảnh lưu trong db
-            $duongdan = "../../images/products/" . $hinhanh; //nơi lưu file upload
-            move_uploaded_file($_FILES["fhinhanh"]["tmp_name"], $duongdan);
-            //xử lý thêm mặt hàng
-            $nguoidungmoi = new NGUOIDUNG();
-            $nguoidungmoi->setemail($_POST["txtemail"]);
-            $nguoidungmoi->setsodienthoai($_POST["txtsodienthoai"]);
-            $nguoidungmoi->setdiachi($_POST["txtdiachi"]);
-            $nguoidungmoi->setmatkhau($_POST["txtmatkhau"]);
-            $nguoidungmoi->sethoten($_POST["txthoten"]);
-            $nguoidungmoi->setloai($loai);
-            $nguoidungmoi->settrangthai($_POST["txttrangthai"]);
-            $nguoidungmoi->sethinhanh($hinhanh);
-            // thêm
-            $nd->themnguoidung($nguoidungmoi);
-            // load người dùng
-            $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($_POST["txtemail"]);
-            include("profile.php");
-            break;
+            $hinhanh = "images/products/" . basename($_FILES["filehinhanh"]["name"]); // đường dẫn ảnh lưu trong db
+            $spsua->sethinhanh($hinhanh);
+            $duongdan = "../../" . $hinhanh; //nơi lưu file upload
+            move_uploaded_file($_FILES["filehinhanh"]["tmp_name"], $duongdan);
+        }
+        // sửa
+        $sp->suasanpham($spsua);
+        // load danh sách
+        $sanphamhh = $sp->laysanphamhethang();
+        include("main.php");
+        break;
+    case "dangky":
+        include("register.php");
+        break;
+    case "xldangky":
+        $loai = $_POST["txtloai"];
+        //xử lý load ảnh
+        $hinhanh = basename($_FILES["fhinhanh"]["name"]); // đường dẫn ảnh lưu trong db
+        $duongdan = "../../images/products/" . $hinhanh; //nơi lưu file upload
+        move_uploaded_file($_FILES["fhinhanh"]["tmp_name"], $duongdan);
+        //xử lý thêm mặt hàng
+        $nguoidungmoi = new NGUOIDUNG();
+        $nguoidungmoi->setemail($_POST["txtemail"]);
+        $nguoidungmoi->setsodienthoai($_POST["txtsodienthoai"]);
+        $nguoidungmoi->setdiachi($_POST["txtdiachi"]);
+        $nguoidungmoi->setmatkhau($_POST["txtmatkhau"]);
+        $nguoidungmoi->sethoten($_POST["txthoten"]);
+        $nguoidungmoi->setloai($loai);
+        $nguoidungmoi->settrangthai($_POST["txttrangthai"]);
+        $nguoidungmoi->sethinhanh($hinhanh);
+        // thêm
+        $nd->themnguoidung($nguoidungmoi);
+        // load người dùng
+        $_SESSION["nguoidung"] = $nd->laythongtinnguoidung($_POST["txtemail"]);
+        $sanphamhh = $sp->laysanphamhethang();
+        include("main.php");
+        break;
     default:
         break;
 }
